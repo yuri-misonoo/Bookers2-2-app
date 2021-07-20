5a0fb3fb83c34376cd9a1868caff1d582a33d5ed
yuri-misonoo <yuri.panpanda@gmail.com>
2021-07-20 13:14:56 +0000

    [add]userのshowページ、左側の部分テンプレート

--- Gemfile
+++ Gemfile
@@ -67,3 +67,6 @@ gem 'bootstrap', '~> 4.5'
 gem 'jquery-rails'
 gem 'font-awesome-sass', '~> 5.13'
 
+
+gem "refile", require: "refile/rails", github: 'manfe/refile'
+gem "refile-mini_magick"
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,32 @@
 class BooksController < ApplicationController
+  
+  def new
+    @book = Book.new
+  end
+  
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(@book.id)
+  end
+  
+  def index
+  end
+  
+  def edit
+  end
+  
+  def update
+  end
+  
+  def destroy
+  end
+  
+  private
+  
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+  
 end
