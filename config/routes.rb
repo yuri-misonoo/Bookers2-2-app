2b535b70509d54e93fd2bf6e614821ece1955211
yuri-misonoo <yuri.panpanda@gmail.com>
2021-07-20 07:24:39 +0000

    [add]top, aboutページ

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: 'homes#top'
-  get 'homes/about' => 'homes#top'
+  get 'homes/about' => 'homes#about'
   resources :users, only: [:index, :show, :edit, :uodate]
   resources :books, only: [:new, :create, :index, :show, :edit, :update]
 
