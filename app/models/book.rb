f1c493348b27e3b4ac6e7f11e553cd5989cecad7
yuri-misonoo <yuri.panpanda@gmail.com>
2021-08-04 02:11:22 +0000

    [add]いいね機能

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,9 +3,10 @@ class Book < ApplicationRecord
   belongs_to :user
   
   has_many :favorites, dependent: :destroy
+  has_many :book_comments, dependent: :destroy
   
   def favorited_by?(user)
-    favorites.where(user_id: user.id).exits?
+    favorites.where(user_id: user.id).exists?
   end
   
   validates :title, presence: true
